e308f1c6306d26e3dbb238bdf27ab723b22d0fe7
Johannes Boehm <johannes.boehm1@swisscom.com>
2024-02-15 07:17:16 +0100

    Added final outcome data structures

---
+++ internal/dataloader/visualstructs.go
@@ -0,0 +1,58 @@
+package dataloader
+
+type GraphContent struct {
+	Nodes []Node `json:"nodes"`
+	Edges []Edge `json:"edges"`
+}
+type Node struct {
+	Id       string            `json:"id"`
+	NodeType PlaybookTaskType  `json:"type"`
+	Data     map[string]string `json:"data"`
+	Position NodePosition      `json:"position"`
+}
+
+type NodePosition struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+type ChildNode struct {
+	Id         string            `json:"id"`
+	NodeType   PlaybookTaskType  `json:"type"`
+	Data       map[string]string `json:"data"`
+	Position   NodePosition      `json:"position"`
+	ParentNode string            `json:"parentNode"`
+	Extent     string            `json:"extent"`
+}
+
+type Edge struct {
+	Id     string `json:"id"`
+	Source string `json:"source"`
+	Target string `json:"target"`
+}
+
+func CreateNode(id string, nodeType PlaybookTaskType, data map[string]string) *Node {
+	return &Node{
+		Id:       id,
+		NodeType: nodeType,
+		Data:     data,
+		Position: NodePosition{
+			X: 0,
+			Y: 0,
+		},
+	}
+}
+
+func CreateChildeNode(id, parentId string, nodeType PlaybookTaskType, data map[string]string) *ChildNode {
+	return &ChildNode{
+		Id:       id,
+		NodeType: nodeType,
+		Data:     data,
+		Position: NodePosition{
+			X: 0,
+			Y: 0,
+		},
+		ParentNode: parentId,
+		Extent:     "parent",
+	}
+}
